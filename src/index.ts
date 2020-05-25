132a3e9557d41da1e7672e357a3024f087fdc682
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-05-25 14:52:27 +0300

    Try to fix module imports/exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import { Picture, PictureSmart } from './react/Picture';
-import Plugin from './webpack/plugin';
-import { getSrcSetString, getBreakpointMedia, getPictureData } from './utils/utils'
 import loader from './webpack/loader';
 
-module.exports.Picture = Picture
-module.exports.PictureSmart = PictureSmart
-
-module.exports.Plugin = Plugin
-
-module.exports.getSrcSetString = getSrcSetString
-module.exports.getBreakpointMedia = getBreakpointMedia
-module.exports.getPictureData = getPictureData
-
-module.exports = loader
+export { Picture, PictureSmart } from './react/Picture';
+export { default as Plugin } from './webpack/plugin';
+export { getSrcSetString, getBreakpointMedia, getPictureData } from './utils/utils';
+export default loader
